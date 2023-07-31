d66efb917385a8cdcfada26c7c32c49eac98734f
Ujjwal Kumar NIraula <106484279+Milan-Niraula@users.noreply.github.com>
2023-07-31 11:47:59 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material'
+import { useSelector, useDispatch } from 'react-redux';
+import { incNumber, decNumber } from './actions/Index';
 
 function App() {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container my-3">
+        <h1>React Material UI learning.</h1>
+        <h3 style={{ color: 'blue' }}>Increment/Decrement Counter</h3>
+        <div className="container my-3">
+          <Button variant="contained" color='primary' onClick={() => dispatch(decNumber())}>
+            <span>-</span>
+          </Button>
+          <input type='text' style={{ textAlign: 'center', width: '50px' }} value={myState}></input>
+          <Button variant="contained" color='primary' onClick={() => dispatch(incNumber())}>
+            <span>+</span>
+          </Button>
+        </div>
+      </div>
+    </>
   );
 }
 
